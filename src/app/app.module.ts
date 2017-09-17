87b1da5ad0c30f617b48391b39d8e694915c34ee
Shauren <shauren.trinity@gmail.com>
2017-09-17 17:25:17 +0200

    Added electron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxElectronModule } from 'ngx-electron';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        NgxElectronModule
     ],
     providers: [],
     bootstrap: [AppComponent]
