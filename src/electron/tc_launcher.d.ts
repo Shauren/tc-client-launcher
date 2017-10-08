af54b73d7a37578b44f80a1e0712b40f58be2e5d
Shauren <shauren.trinity@gmail.com>
2017-10-08 18:09:05 +0200

    Expose encryption and decryption from native code

--- src/electron/tc_launcher.d.ts
+++ src/electron/tc_launcher.d.ts
@@ -1,3 +1,5 @@
 interface Launcher {
     launchGame(gameInstallDir: string, use64Bit: boolean, portal: string, loginTicket: string, gameAccount: string): boolean;
+    encryptString(inputString: string): Buffer;
+    decryptString(encryptedString: Buffer): string;
 }
--- src/native/MacLauncher.cpp
+++ src/native/MacLauncher.cpp
@@ -1,6 +1,16 @@
 
 #include "LauncherShared.h"
 
+bool EncryptString(char const* string, std::vector<uint8_t>* output)
+{
+    return false;
+}
+
+bool DecryptString(std::vector<uint8_t> const& encryptedString, std::string* output)
+{
+    return false;
+}
+
 bool StoreLoginTicket(char const* portal, char const* loginTicket, char const* gameAccount)
 {
     return false;
