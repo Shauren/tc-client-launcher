3d2e938498717a7bb477d21bc7af89d0b3615858
Shauren <shauren.trinity@gmail.com>
2022-09-12 21:37:55 +0200

    Support node 16

--- src/native/Launcher.cpp
+++ src/native/Launcher.cpp
@@ -12,21 +12,23 @@ void LaunchGame(v8::FunctionCallbackInfo<v8::Value> const& args)
 
     if (args.Length() < 4)
     {
-        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments, expected 5")));
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments, expected 5", v8::NewStringType::kNormal)
+            .ToLocalChecked()));
         return;
     }
 
     if (!args[0]->IsString() || !args[1]->IsString() || !args[2]->IsString() || !args[3]->IsString())
     {
         isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate,
-                "Wrong arguments types, expected (gameInstallDir: string, portal: string, loginTicket: string, gameAccount: string)")));
+                "Wrong arguments types, expected (gameInstallDir: string, portal: string, loginTicket: string, gameAccount: string)", v8::NewStringType::kNormal)
+            .ToLocalChecked()));
         return;
     }
 
-    v8::String::Utf8Value gameInstallDir(isolate,  args[0]->ToString(isolate));
-    v8::String::Utf8Value portal(isolate, args[1]->ToString(isolate));
-    v8::String::Utf8Value loginTicket(isolate, args[2]->ToString(isolate));
-    v8::String::Utf8Value gameAccount(isolate, args[3]->ToString(isolate));
+    v8::String::Utf8Value gameInstallDir(isolate,  args[0]->ToString(isolate->GetCurrentContext()).ToLocalChecked());
+    v8::String::Utf8Value portal(isolate, args[1]->ToString(isolate->GetCurrentContext()).ToLocalChecked());
+    v8::String::Utf8Value loginTicket(isolate, args[2]->ToString(isolate->GetCurrentContext()).ToLocalChecked());
+    v8::String::Utf8Value gameAccount(isolate, args[3]->ToString(isolate->GetCurrentContext()).ToLocalChecked());
 
     bool success = false;
     if (StoreLoginTicket(*portal, *loginTicket, *gameAccount))
@@ -43,23 +45,23 @@ void EncryptJsString(v8::FunctionCallbackInfo<v8::Value> const& args)
 
     if (args.Length() != 1)
     {
-        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments, expected 1")));
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments, expected 1", v8::NewStringType::kNormal).ToLocalChecked()));
         return;
     }
 
     if (!args[0]->IsString())
     {
         isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate,
-            "Wrong arguments types, expected (inputString: string)")));
+            "Wrong arguments types, expected (inputString: string)", v8::NewStringType::kNormal).ToLocalChecked()));
         return;
     }
 
-    v8::String::Utf8Value inputString(isolate, args[0]->ToString(isolate));
+    v8::String::Utf8Value inputString(isolate, args[0]->ToString(isolate->GetCurrentContext()).ToLocalChecked());
     std::unique_ptr<std::vector<uint8_t>> encryptedString = std::make_unique<std::vector<uint8_t>>();
 
     if (!EncryptString(*inputString, encryptedString.get()))
     {
-        isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Encryption failed")));
+        isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Encryption failed", v8::NewStringType::kNormal).ToLocalChecked()));
         return;
     }
 
@@ -70,7 +72,7 @@ void EncryptJsString(v8::FunctionCallbackInfo<v8::Value> const& args)
     if (node::Buffer::New(isolate, data, length, deleter, encryptedString.release()).ToLocal(&returnBuffer))
         args.GetReturnValue().Set(returnBuffer);
     else
-        isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Output buffer creation failed")));
+        isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Output buffer creation failed", v8::NewStringType::kNormal).ToLocalChecked()));
 }
 
 void DecryptJsString(v8::FunctionCallbackInfo<v8::Value> const& args)
@@ -80,25 +82,26 @@ void DecryptJsString(v8::FunctionCallbackInfo<v8::Value> const& args)
 
     if (args.Length() != 1)
     {
-        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments, expected 1")));
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Wrong number of arguments, expected 1", v8::NewStringType::kNormal)
+            .ToLocalChecked()));
         return;
     }
 
     if (!args[0]->IsObject())
     {
         isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate,
-            "Wrong arguments types, expected (encryptedString: Buffer)")));
+            "Wrong arguments types, expected (encryptedString: Buffer)", v8::NewStringType::kNormal).ToLocalChecked()));
         return;
     }
 
-    v8::Local<v8::Object> encryptedString = args[0]->ToObject(isolate);
+    v8::Local<v8::Object> encryptedString = args[0]->ToObject(isolate->GetCurrentContext()).ToLocalChecked();
     uint8_t* data = reinterpret_cast<uint8_t*>(node::Buffer::Data(encryptedString));
     size_t length = node::Buffer::Length(encryptedString);
     std::string outputString;
 
     if (!DecryptString(std::vector<uint8_t>(data, data + length), &outputString))
     {
-        isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Decryption failed")));
+        isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate, "Decryption failed", v8::NewStringType::kNormal).ToLocalChecked()));
         return;
     }
 
@@ -106,7 +109,7 @@ void DecryptJsString(v8::FunctionCallbackInfo<v8::Value> const& args)
     if (v8::String::NewFromUtf8(isolate, outputString.c_str(), v8::NewStringType::kNormal, outputString.length()).ToLocal(&returnString))
         args.GetReturnValue().Set(returnString);
     else
-        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Output string creation failed")));
+        isolate->ThrowException(v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, "Output string creation failed", v8::NewStringType::kNormal).ToLocalChecked()));
 }
 
 NODE_MODULE_INIT(/*exports, module, context*/)
